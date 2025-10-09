7c5cd2555794bc51680c0f2cc9a223eb84739e69
prabha143-lab <prabhakarreddykonu@gmail.com>
2025-10-09 17:11:13 +0530

    Ok

--- Python Scripts/Python Data Structures List, Tuple, Set, Dictionary/tuple_access_add_bulk_remove_filter_demo_for_interview_and_mentoring.py
+++ Python Scripts/Python Data Structures List, Tuple, Set, Dictionary/tuple_access_add_bulk_remove_filter_demo_for_interview_and_mentoring.py
@@ -50,3 +50,22 @@ filtered_out = tuple(item for item in my_tuple if item >= 18)
 # Keep only values less than 18 using filter()
 my_tuple = tuple(filter(lambda item: item < 18, my_tuple))
 print(f"\nFiltered out values >= 18 {filtered_out} =>", my_tuple)
+
+
+****************************OUTPUT ************************
+
+
+Access first two elements => (1, 2)
+Slice first two elements => (1, 2)
+
+Expanded tuple => (1, 2, -1, 3, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+
+Removed values (3, 9, 13, 15, 20) => (1, 2, -1, 8, 10, 11, 12, 14, 16, 17, 18, 19)
+
+Removed slice [2:6] => (-1, 8, 10, 11, 12) => (1, 2, 12, 14, 16, 17, 18, 19)
+
+Removed odd numbers (1, 17, 19) => (2, 12, 14, 16, 18)
+
+Filtered out values >= 18 (18,) => (2, 12, 14, 16)
+
+=== Code Execution Successful ===
