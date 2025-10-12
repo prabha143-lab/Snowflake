635b66855972495b524b80779eb374b87a95f2f2
prabha143-lab <prabhakarreddykonu@gmail.com>
2025-10-12 14:43:25 +0530

    Ok

--- Snowflake SPLIT functions/Extract_Name_Components_With_SPLIT_SPLIT_PART_REGEXP_SUBSTR_REPLACE_In_Snowflake.sql
+++ Snowflake SPLIT functions/Extract_Name_Components_With_SPLIT_SPLIT_PART_REGEXP_SUBSTR_REPLACE_In_Snowflake.sql
@@ -81,3 +81,116 @@ RAJESH KUMAR REDDY	RAJESH	KUMAR	REDDY
 RAJESH	RAJESH		
 APARNA PAUL	APARNA	PAUL	
 KOTLA-ROJA-REDDY	KOTLA	ROJA	REDDY
+
+
+****************************************************************
+
+CREATE OR REPLACE TABLE input_names (name STRING);
+
+INSERT INTO input_names (name) VALUES('RAJESH KUMAR REDDY'),('RAJESH'),('APARNA PAUL'),('KOTLA-ROJA-REDDY');
+
+SELECT 
+    name,
+    CASE WHEN ARRAY_SIZE(SPLIT(REPLACE(name, '-', ' '), ' ')) >= 1 THEN SPLIT_PART(REPLACE(name, '-', ' '), ' ', 1) ELSE NULL END AS COL1,
+    CASE WHEN ARRAY_SIZE(SPLIT(REPLACE(name, '-', ' '), ' ')) >= 2 THEN SPLIT_PART(REPLACE(name, '-', ' '), ' ', 2) ELSE NULL END AS COL2,
+    CASE WHEN ARRAY_SIZE(SPLIT(REPLACE(name, '-', ' '), ' ')) >= 3 THEN SPLIT_PART(REPLACE(name, '-', ' '), ' ', 3) ELSE NULL END AS COL3 
+FROM input_names;
+
+
+NAME	COL1	COL2	COL3
+RAJESH KUMAR REDDY	RAJESH	KUMAR	REDDY
+RAJESH	RAJESH		
+APARNA PAUL	APARNA	PAUL	
+KOTLA-ROJA-REDDY	KOTLA	ROJA	REDDY
+
+******************************************************************
+
+
+
+CREATE OR REPLACE TABLE input_names (name STRING);
+-- Step 2: Insert the data
+INSERT INTO input_names (name)
+VALUES('RAJESH KUMAR REDDY'),('RAJESH'),('APARNA PAUL'),('KOTLA-ROJA-REDDY');
+
+SELECT 
+    name,
+    OBJECT_CONSTRUCT('COL1', SPLIT_PART(REPLACE(name, '-', ' '), ' ', 1),
+                     'COL2', SPLIT_PART(REPLACE(name, '-', ' '), ' ', 2),
+                     'COL3', SPLIT_PART(REPLACE(name, '-', ' '), ' ', 3)) AS name_parts
+FROM input_names;
+
+
+
+NAME	NAME_PARTS
+RAJESH KUMAR REDDY	{
+  "COL1": "RAJESH",
+  "COL2": "KUMAR",
+  "COL3": "REDDY"
+}
+RAJESH	{
+  "COL1": "RAJESH",
+  "COL2": "",
+  "COL3": ""
+}
+APARNA PAUL	{
+  "COL1": "APARNA",
+  "COL2": "PAUL",
+  "COL3": ""
+}
+KOTLA-ROJA-REDDY	{
+  "COL1": "KOTLA",
+  "COL2": "ROJA",
+  "COL3": "REDDY"
+}
+
+
+
+****************************************************************
+
+
+CREATE OR REPLACE TABLE input_names (name STRING);
+-- Step 2: Insert the data
+INSERT INTO input_names (name)
+VALUES('RAJESH KUMAR REDDY'),('RAJESH'),('APARNA PAUL'),('KOTLA-ROJA-REDDY');
+
+
+SELECT 
+    name,
+    VALUE AS part,
+    SEQ AS position
+FROM input_names,
+LATERAL FLATTEN(input => SPLIT(REPLACE(name, '-', ' '), ' '));
+
+NAME	PART	POSITION
+RAJESH KUMAR REDDY	"RAJESH"	1
+RAJESH KUMAR REDDY	"KUMAR"	1
+RAJESH KUMAR REDDY	"REDDY"	1
+RAJESH	"RAJESH"	2
+APARNA PAUL	"APARNA"	3
+APARNA PAUL	"PAUL"	3
+KOTLA-ROJA-REDDY	"KOTLA"	4
+KOTLA-ROJA-REDDY	"ROJA"	4
+KOTLA-ROJA-REDDY	"REDDY"	4
+
+
+*********************************************************************
+
+
+CREATE OR REPLACE TABLE split_input (id INT, raw_string STRING );
+
+INSERT INTO split_input (id, raw_string) VALUES (1, 'a.b'), (2, 'c*d*e'), (3,'f-g-h')
+
+SELECT 
+    split_input.id, table1.value
+FROM split_input,
+TABLE(SPLIT_TO_TABLE(replace(raw_string,'*','.'), '.')) AS table1
+ORDER BY table1.value;
+
+
+ID	VALUE
+1	a
+1	b
+2	c
+2	d
+2	e
+3	f-g-h
