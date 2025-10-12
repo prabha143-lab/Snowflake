dd24993ccf748fefc922be6e1f0c95b4bef0e5c0
prabha143-lab <prabhakarreddykonu@gmail.com>
2025-10-12 10:36:46 +0530

    Ok

---
+++ Snowsql-COPY-SNOWPIPE/sp_load_employee_data2_snowpipe_test.sql
@@ -0,0 +1,24 @@
+-- Stored Procedure: Load employee data from stage to employee_data2_pipe_test
+CREATE OR REPLACE PROCEDURE sp_load_employee_data2_snowpipe_test()
+RETURNS STRING
+LANGUAGE SQL
+AS
+$$
+DECLARE
+    pipe_status STRING;
+BEGIN
+    CREATE OR REPLACE PIPE employee_data2_pipe_test
+    AUTO_INGEST = FALSE
+    AS
+    COPY INTO employee_data2_snowpipe_test
+    FROM @employee_data2_snowpipe_test/employee_data2_snowpipe_test.csv.gz
+    FILE_FORMAT = (TYPE = 'CSV' FIELD_OPTIONALLY_ENCLOSED_BY = '"' SKIP_HEADER = 1)
+    ON_ERROR = 'CONTINUE';  -- Optional: skip bad records
+    
+	ALTER PIPE employee_data2_pipe_test REFRESH;
+	
+    -- Assign success message using := syntax
+    pipe_status := 'Data load completed into employee_data2_pipe_test.';
+    RETURN pipe_status;
+END;
+$$;
